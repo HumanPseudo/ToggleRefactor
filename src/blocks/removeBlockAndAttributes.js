35e22427c382b9521744ee942fc02cb531577bca
ValenMorales <valentina.morales@kommit.co>
2024-09-11 10:03:35 -0500

    union of functions with similar logic in a same file

--- src/blocks/removeBlockAndAttributes.js
+++ src/blocks/removeBlockAndAttributes.js
@@ -1,4 +1,17 @@
-/**
+ /**
+   * When a nested block is removed, the 'items' attribute
+   * is updated, subtracting from it an unit.
+   * @param {string} id - block identifier
+   */
+ export function removeBlock(holder, id, cursorPosition) {
+    if (cursorPosition === 0) {
+      const position = this.itemsId.indexOf(id);
+      this.itemsId.splice(position, 1);
+    }
+  }
+  
+
+  /**
    * Removes all properties of a nested block.
    *
    * @param {number} destiny - block position
